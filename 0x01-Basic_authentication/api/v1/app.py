30f32125f335bf94c739c73bf323d3421be53489
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-16 15:50:37 +0300

    created basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,21 +15,25 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE')
 if auth_type:
-    #Auth = __import__('api.v1.auth.auth').Auth
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 
 @app.before_request
 def before_request_func():
     """Execute before request
     """
     if auth:
-        check = auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+        check = auth.require_auth(request.path,
+                                  ['/api/v1/status/',
+                                   '/api/v1/unauthorized/',
+                                   '/api/v1/forbidden/'])
         if check is False:
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -37,17 +41,20 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """forbidden
     """
     return jsonify({"error": "Forbidden"}), 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
