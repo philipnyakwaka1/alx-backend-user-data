b8dde3fa783040466277187d00b5455c572712ab
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-23 19:33:06 +0300

    destroy session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,12 +46,12 @@ def login() -> str:
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
-    session_id = request.cookies.get('session_id', None)
+    session_id = request.cookies.get("session_id", None)
     user = auth.get_user_from_session_id(session_id)
-    if user is None:
+    if user is None or session_id is None:
         abort(403)
     auth.destroy_session(user.id)
-    return redirect('/')
+    return redirect("/")
 
 
 if __name__ == '__main__':
