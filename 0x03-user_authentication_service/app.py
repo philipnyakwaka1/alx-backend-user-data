2a62843b6cf8acfe983c62054730abb55dfe8033
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-23 12:16:41 +0300

    implemented register user url

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,31 @@
 """
 Flask application
 """
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from typing import List, Tuple
+from auth import Auth
+
+
 app = Flask(__name__)
+auth = Auth()
 
 
-@app.route('/', strict_slashes=False)
-def home_page(methods: List[str] = ['GET']) -> Tuple[dict, int]:
+@app.route('/', strict_slashes=False, methods=['GET'])
+def home_page() -> Tuple[dict, int]:
     return jsonify({"message": "Bienvenue"}), 200
 
 
+@app.route('/users', strict_slashes=False, methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = auth.register_user(email, password)
+        return jsonify(
+            {"email": f"{user.email}", "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
