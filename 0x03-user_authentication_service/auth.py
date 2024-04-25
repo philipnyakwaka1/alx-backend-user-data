9e6883e850fe2794800a2d1f0ef45c1ab02d3a44
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-25 11:48:09 +0300

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,7 +87,17 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
+        self._db.update_user(user.id, reset_token=_generate_uuid())
+        return user.reset_token
 
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Returns password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_p = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_p, reset_token=None)
+        return None
