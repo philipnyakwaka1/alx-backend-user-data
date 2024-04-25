493dbff43a680b995e6e071aa4b550c6341c63c8
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-25 11:33:58 +0300

    reset_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,5 +87,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
-        self._db.update_user(user.id, reset_token=_generate_uuid())
-        return user.reset_token
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
