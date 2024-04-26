aa33d7514ec2f8c24f01fcace9421be48932628f
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-26 21:40:11 +0300

    run tests

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -5,131 +5,76 @@ Main file
 import requests
 
 
-
 def register_user(email: str, password: str) -> None:
+    """Tests register user end-point
     """
-    Test for register a user with the given email and password.
-    Args:
-        email: The email of the user.
-        password: The password of the user.
-    Returns:
-        None
-    """
-    resp = requests.post('http://127.0.0.1:5000/users',
-                         data={'email': email, 'password': password})
-    if resp.status_code == 200:
-        assert (resp.json() == {"email": email, "message": "user created"})
-    else:
-        assert(resp.status_code == 400)
-        assert (resp.json() == {"message": "email already registered"})
+    payload = {'email': email, 'password': password}
+    resp = requests.post('http://localhost:5000/users', data=payload)
+    assert resp.status_code == 200
+    assert resp.json().get('email') == email
+    dup = resp = requests.post('http://localhost:5000/users', data=payload)
+    assert resp.status_code == 400
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
+    """Tests for incorrect credentials login
     """
-    Test for log in with the given wrong credentials.
-    Args:
-        email: The email of the user.
-        password: The password of the user.
-    Returns:
-        None
-    """
-    r = requests.post('http://127.0.0.1:5000/sessions',
-                      data={'email': email, 'password': password})
-    assert (r.status_code == 401)
-
-
-def profile_unlogged() -> None:
-    """
-    Test for profile without being logged in with session_id.
-    Returns:
-        None
-    """
-    r = requests.get('http://127.0.0.1:5000/profile')
-    assert(r.status_code == 403)
+    payload = {'email': email, 'password': password}
+    resp = requests.post('http://localhost:5000/sessions', data=payload)
+    assert resp.status_code == 401
 
 
 def log_in(email: str, password: str) -> str:
+    """Tests valid login
     """
-    Test for log in with the given correct email and password.
-    Args:
-        email: The email of the user.
-        password: The password of the user.
-    Returns:
-        The session_id of the user.
+    payload = {'email': email, 'password': password}
+    resp = requests.post('http://localhost:5000/sessions', data=payload)
+    assert resp.status_code == 200
+    assert resp.json().get('message') == 'logged in'
+    return resp.cookies.get('session_id')
+
+
+def profile_unlogged() -> None:
+    """Tests unlogged profile
     """
-    resp = requests.post('http://127.0.0.1:5000/sessions',
-                         data={'email': email, 'password': password})
-    assert (resp.status_code == 200)
-    assert(resp.json() == {"email": email, "message": "logged in"})
-    return resp.cookies['session_id']
+    resp = requests.get('http://localhost:5000/profile')
+    assert resp.status_code == 403
 
 
 def profile_logged(session_id: str) -> None:
+    """Tests for logged profile
     """
-    Test for profile with being logged in with session_id.
-    Args:
-        session_id: The session_id of the user.
-    Returns:
-        None
-    """
-    cookies = {'session_id': session_id}
-    r = requests.get('http://127.0.0.1:5000/profile',
-                     cookies=cookies)
-    assert(r.status_code == 200)
+    cookies = dict(session_id=session_id)
+    resp = requests.get('http://localhost:5000/profile', cookies=cookies)
+    assert resp.status_code == 200
 
 
 def log_out(session_id: str) -> None:
+    """Tests for logout
     """
-    Test for log out with the given session_id.
-    Args:
-        session_id: The session_id of the user.
-    Returns:
-        None
-    """
-    cookies = {'session_id': session_id}
-    r = requests.delete('http://127.0.0.1:5000/sessions',
-                        cookies=cookies)
-    if r.status_code == 302:
-        assert(r.url == 'http://127.0.0.1:5000/')
-    else:
-        assert(r.status_code == 200)
+    cookies = dict(session_id=session_id)
+    resp = requests.delete('http://localhost:5000/sessions', cookies=cookies)
+    assert resp.json().get('message') == 'Bienvenue'
+    assert resp.status_code == 200
 
 
 def reset_password_token(email: str) -> str:
+    """tests for reset password token
     """
-    Test for reset password token with the given email.
-    Args:
-        email: The email of the user.
-    Returns:
-        The reset_token of the user.
-    """
-    r = requests.post('http://127.0.0.1:5000/reset_password',
-                      data={'email': email})
-    if r.status_code == 200:
-        return r.json()['reset_token']
-    assert(r.status_code == 401)
+    payload = {'email': email}
+    resp = requests.post('http://localhost:5000/reset_password', data=payload)
+    assert resp.status_code == 200
+    assert resp.json().get('reset_token') is not None
 
 
-def update_password(email: str, reset_token: str,
-                    new_password: str) -> None:
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Tests for update password
     """
-    Test for update password with the given email,
-    reset_token and new_password.
-    Args:
-        email: The email of the user.
-        reset_token: The reset_token of the user.
-        new_password: The new password of the user.
-    Returns:
-        None
-    """
-    data = {'email': email, 'reset_token': reset_token,
-            'new_password': new_password}
-    r = requests.put('http://127.0.0.1:5000/reset_password',
-                     data=data)
-    if r.status_code == 200:
-        assert(r.json() == {"email": email, "message": "Password updated"})
-    else:
-        assert(r.status_code == 403)
+    payload = {'email': email,
+               'reset_token': reset_token, 'new_password': new_password}
+    resp = requests.put('http://localhost:5000/reset_password', data=payload)
+    assert resp.status_code == 200
+    assert resp.json().get('message') == 'Password updated'
 
 
 EMAIL = "guillaume@holberton.io"
@@ -145,6 +90,3 @@ if __name__ == "__main__":
     session_id = log_in(EMAIL, PASSWD)
     profile_logged(session_id)
     log_out(session_id)
-    reset_token = reset_password_token(EMAIL)
-    update_password(EMAIL, reset_token, NEW_PASSWD)
-    log_in(EMAIL, NEW_PASSWD)
